be776b12cd9de81a770946cec4018869a33fed22
borleadaniel <38912528+borleadaniel@users.noreply.github.com>
2020-03-08 20:38:21 +0200

    initial commit

---
+++ MyControl.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Simulator
+{
+    /// <summary>
+    /// Interaction logic for MyControl.xaml
+    /// </summary>
+    public partial class MyControl : UserControl
+    {
+        ViewModel _vm;
+        public MyControl()
+        {
+            InitializeComponent();
+            _vm = new ViewModel();
+            this.DataContext = _vm;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            _vm.IsRedForCar = !_vm.IsRedForCar;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            _vm.IsYellowForCar = !_vm.IsYellowForCar;
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            _vm.IsGreenForCar = !_vm.IsGreenForCar;
+        }
+
+        private void Button_Click4(object sender, RoutedEventArgs e)
+        {
+            _vm.IsRedForPeople = !_vm.IsRedForPeople;
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            _vm.IsGreenForPeople = !_vm.IsGreenForPeople;
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_S0(object sender, RoutedEventArgs e)
+        {
+            _vm.ForceNextState(ProcessState.Off);
+        }
+
+        private void Button_Click_S1(object sender, RoutedEventArgs e)
+        {
+            _vm.ForceNextState(ProcessState.AutoYellow);
+        }
+
+        private void Button_Click_S2(object sender, RoutedEventArgs e)
+        {
+            _vm.ForceNextState(ProcessState.BlinkOn);
+        }
+
+        private void Button_Click_S3(object sender, RoutedEventArgs e)
+        {
+            _vm.ForceNextState(ProcessState.AutoRed);
+        }
+    }
+}
---
+++ ViewModel.cs
@@ -0,0 +1,311 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace Simulator
+{
+    public enum ButtonState
+    { 
+        Off,
+        Request,
+        Blink,
+        Flash,
+    }
+
+    public enum ProcessState
+    {
+        Off,
+        BlinkOn,
+        BlinkOff,     
+        AutoRed,
+        AutoYellow,
+        AutoGreen,
+        AutoRequest,      
+    }
+    
+    class ViewModel : INotifyPropertyChanged
+    {
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChanged(string prop)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+
+        private BackgroundWorker _worker = new BackgroundWorker();
+        private System.Timers.Timer _timer = new System.Timers.Timer();
+
+        private ProcessState _theStateOfTheProcess = ProcessState.Off;
+
+
+        public ViewModel()
+        {
+            _timer.Elapsed += _timer_Elapsed;
+            _worker.DoWork += _worker_DoWork;
+            _worker.RunWorkerAsync();
+
+        }
+      
+        private void _timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            _isEventRaised = false;
+            _theStateOfTheProcess = _nextState;
+            _timer.Stop();
+        }
+
+
+        private bool _isEventRaised = false;
+        private ProcessState _nextState;
+        private void RaiseTimerEvent(ProcessState NextProcessState, int TimeInterval)
+        {
+            if(!_isEventRaised)
+            {
+                _isEventRaised = true;
+                _nextState = NextProcessState;
+                _timer.Interval = TimeInterval;
+                _timer.Start();
+            }
+        }
+
+
+
+        private void _worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while(true)
+            {              
+                ProcessNextState(_theStateOfTheProcess);
+                System.Threading.Thread.Sleep(100);
+            }
+        }
+
+
+        public void ProcessNextState(ProcessState CurrentState)
+        {
+            switch (CurrentState)
+            {
+                case ProcessState.Off:
+                    IsRedForCar = false;
+                    IsYellowForCar = false;
+                    IsGreenForCar = false;
+                    IsRedForPeople = false;
+                    IsGreenForPeople = false;
+
+                    RaiseTimerEvent(ProcessState.Off, 1000);
+                   
+                    break;
+                case ProcessState.BlinkOn:
+                    IsRedForCar = false;
+                    IsYellowForCar = true;
+                    IsGreenForCar = false;
+                    IsRedForPeople = false;
+                    IsGreenForPeople = false;
+
+                    RaiseTimerEvent(ProcessState.BlinkOff, 2000);
+
+                    break;
+                case ProcessState.BlinkOff:
+                    IsRedForCar = false;
+                    IsYellowForCar = false;
+                    IsGreenForCar = false;
+                    IsRedForPeople = false;
+                    IsGreenForPeople = false;
+                    
+                    RaiseTimerEvent(ProcessState.BlinkOn, 2000);
+                   
+                    break;               
+                case ProcessState.AutoRed:
+                    IsRedForCar = true;
+                    IsYellowForCar = false;
+                    IsGreenForCar = false;
+                    IsRedForPeople = false;
+                    IsGreenForPeople = true;
+
+                    RaiseTimerEvent(ProcessState.AutoGreen, 5000);
+
+                    break;
+                case ProcessState.AutoYellow:
+                    IsRedForCar = false;
+                    IsYellowForCar = true;
+                    IsGreenForCar = false;
+                    IsRedForPeople = true;
+                    IsGreenForPeople = false;
+                    
+                    RaiseTimerEvent(ProcessState.AutoRed, 3000);
+
+                    break;
+                case ProcessState.AutoGreen:
+                    IsRedForCar = false;
+                    IsYellowForCar = false;
+                    IsGreenForCar = true;
+                    IsRedForPeople = true;
+                    IsGreenForPeople = false;
+
+                    RaiseTimerEvent(ProcessState.AutoYellow, 10000);
+
+                    break;                              
+            }
+
+        }
+
+        public void ForceNextState(ProcessState NextState)
+        {
+            _isEventRaised = false;
+            _timer.Stop();
+            _theStateOfTheProcess = NextState;
+        }
+
+
+        private bool _isRedForCar;
+        public bool IsRedForCar 
+        {
+            get
+            {
+                return _isRedForCar;
+            }
+            set
+            {
+                _isRedForCar = value;
+                OnPropertyChanged(nameof(IsRedForCarsVisible));
+            }
+        }
+
+        public System.Windows.Visibility IsRedForCarsVisible
+        {
+            get
+            {
+                if(_isRedForCar)
+                {
+                    return System.Windows.Visibility.Visible;
+                }
+                else
+                {
+                    return System.Windows.Visibility.Hidden;
+                }
+            }
+        }
+
+        private bool _isYellowForCar;
+        public bool IsYellowForCar
+        {
+            get
+            {
+                return _isYellowForCar;
+            }
+            set
+            {
+                _isYellowForCar = value;
+                OnPropertyChanged(nameof(IsYellowForCarsVisible));
+            }
+        }
+
+        public System.Windows.Visibility IsYellowForCarsVisible
+        {
+            get
+            {
+                if (_isYellowForCar)
+                {
+                    return System.Windows.Visibility.Visible;
+                }
+                else
+                {
+                    return System.Windows.Visibility.Hidden;
+                }
+            }
+        }
+
+        private bool _isGreenForCar;
+        public bool IsGreenForCar
+        {
+            get
+            {
+                return _isGreenForCar;
+            }
+            set
+            {
+                _isGreenForCar = value;
+                OnPropertyChanged(nameof(IsGreenForCarsVisible));
+            }
+        }
+
+        public System.Windows.Visibility IsGreenForCarsVisible
+        {
+            get
+            {
+                if (_isGreenForCar)
+                {
+                    return System.Windows.Visibility.Visible;
+                }
+                else
+                {
+                    return System.Windows.Visibility.Hidden;
+                }
+            }
+        }
+
+        private bool _isGreenForPeople;
+        public bool IsGreenForPeople
+        {
+            get
+            {
+                return _isGreenForPeople;
+            }
+            set
+            {
+                _isGreenForPeople = value;
+                OnPropertyChanged(nameof(IsGreenForPeopleVisible));
+            }
+        }
+
+        public System.Windows.Visibility IsGreenForPeopleVisible
+        {
+            get
+            {
+                if (_isGreenForPeople)
+                {
+                    return System.Windows.Visibility.Visible;
+                }
+                else
+                {
+                    return System.Windows.Visibility.Hidden;
+                }
+            }
+        }
+
+        private bool _isRedForPeople;
+        public bool IsRedForPeople
+        {
+            get
+            {
+                return _isRedForPeople;
+            }
+            set
+            {
+                _isRedForPeople = value;
+                OnPropertyChanged(nameof(IsRedForPeopleVisible));
+            }
+        }
+
+        public System.Windows.Visibility IsRedForPeopleVisible
+        {
+            get
+            {
+                if (_isRedForPeople)
+                {
+                    return System.Windows.Visibility.Visible;
+                }
+                else
+                {
+                    return System.Windows.Visibility.Hidden;
+                }
+            }
+        }
+
+
+
+    }
+}
